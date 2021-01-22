242c0223156c9de72b4bd7b59c0d3eecc56bb400
tillaeva <tillaevajm@gmail.com>
2021-01-22 21:41:21 +0500

    akula i chelovek

---
+++ script.js
@@ -0,0 +1,61 @@
+// loop - sikl
+
+/* let answer
+
+answer = prompt('vvedite bolshe 5')
+while(answer < 5) {
+    answer = prompt('vvedite bolshe 5')
+}
+*/
+
+/*let answer 
+
+do {
+    answer = prompt ('vvedite bolshe 5')
+} while (answer < 5)*/
+
+/*let answer
+
+do {
+    answer =prompt('vvedite sifru')
+} while (isNaN(answer))
+*/
+
+// do {
+//     answer = prompt ('vvedite sifru, no ne dva')
+// } while(answer != 2 ||  isNaN(answer))
+
+// for (let index = 0; index < array.length; index++)
+//      boshlangich     borish tochkasi     qadam kattaligi
+
+// for (let i=0; i< Infinity; i++) {
+//     console.log(i);
+//     if( i >=10) break
+// }
+// let f = 1
+
+// y = prompt('Vvedite sifru')
+
+// for (i=1; i<=y; i++) {
+//     f*=i
+// }
+// console.log(f);
+
+let S1 = prompt ('S chelovek i bereg');
+let S2 = prompt ('S chelovek i akula');
+let d = prompt ('Esli delfin est = 1 , inache 0')
+if (d==false){
+if (3*S1>=S2){
+    alert('Alive');
+}
+else {
+    alert ('Shark bit')
+}}
+else {
+    if(3*S1 <= 2*S2){
+    alert('Alive');
+}
+else {
+    alert ('Shark bit')
+    }
+}
